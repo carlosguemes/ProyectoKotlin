74bc96c1bdaebb1893104a0f06816fa12aecfe40
carlos <carlosguemesdam@gmail.com>
2024-03-01 12:30:25 +0100

    Commit V8: Boton cambio de pantalla entre activitys

--- app/src/main/java/com/example/proyectokotlin/Activity_2.kt
+++ app/src/main/java/com/example/proyectokotlin/Activity_2.kt
@@ -2,10 +2,15 @@ package com.example.proyectokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Activity_2 : AppCompatActivity() {
+
+    lateinit var texto: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
+
+        texto = this.findViewById(R.id.textoBienvenida)
     }
 }
--- app/src/main/java/com/example/proyectokotlin/MainActivity.kt
+++ app/src/main/java/com/example/proyectokotlin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.proyectokotlin
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -34,6 +35,12 @@ class MainActivity : AppCompatActivity() {
         })
 
     }
+
+    fun cambioPantalla(v: View?){
+        var activity_2: Intent = Intent(this, Activity_2::class.java)
+        startActivity(activity_2)
+    }
+
     fun cambiarColor(): Int{
         return Color.rgb(random.nextInt(256),
             random.nextInt(256),
