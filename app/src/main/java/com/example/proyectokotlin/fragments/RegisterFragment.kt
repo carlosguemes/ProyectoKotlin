410108310e5f9afd93bca5784ed783017e8b9356
carlos <carlosguemesdam@gmail.com>
2024-03-02 14:33:16 +0100

    Commit V19: Código completo en el RegisterFragment

--- app/src/main/java/com/example/proyectokotlin/fragments/RegisterFragment.kt
+++ app/src/main/java/com/example/proyectokotlin/fragments/RegisterFragment.kt
@@ -1,5 +1,6 @@
 package com.example.proyectokotlin.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,8 +11,11 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import com.example.proyectokotlin.MainActivity
 import com.example.proyectokotlin.R
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
@@ -49,6 +53,43 @@ class RegisterFragment : Fragment(), OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        TODO("Not yet implemented")
+        when (v?.id) {
+            R.id.botonAceptarRegistro -> {
+                if (checkFields()) {
+                    Snackbar.make(requireView(), "Por favor, completa todos los campos.", Snackbar.LENGTH_SHORT).show()
+                } else {
+                    auth.createUserWithEmailAndPassword(textoEmailRegistro.text.toString(), textoPasswordRegistro.text.toString())
+                        .addOnCompleteListener(requireActivity()) { task ->
+                            if (task.isSuccessful) {
+                                val activity1: Intent = Intent(requireActivity(), MainActivity::class.java)
+                                startActivity(activity1)
+                                requireActivity().finish()
+                            } else {
+                                val exception = task.exception
+                                if (exception is FirebaseAuthException) {
+                                    val errorMessage = when (exception.errorCode) {
+                                        "ERROR_WEAK_PASSWORD" -> "La contraseña es débil. Debe tener al menos 8 caracteres."
+                                        "ERROR_INVALID_CREDENTIAL" -> "Credenciales inválidas. Revisa el formato del correo electrónico."
+                                        "ERROR_EMAIL_ALREADY_IN_USE" -> "Esta cuenta ya está en uso."
+                                        else -> "Error al crear la cuenta. Por favor, inténtalo nuevamente."
+                                    }
+                                    Snackbar.make(requireView(), errorMessage, Snackbar.LENGTH_SHORT).show()
+                                } else {
+                                    Snackbar.make(requireView(), "Error al crear la cuenta. Por favor, inténtalo nuevamente.", Snackbar.LENGTH_SHORT).show()
+                                }
+                            }
+                        }
+                }
+            }
+            R.id.botonCancelarRegistro -> navController.navigate(R.id.action_registerFragment_to_loginFragment)
+        }
+    }
+
+    private fun checkFields(): Boolean {
+        return textoEmailRegistro.text.toString().isEmpty() || textoPasswordRegistro.text.toString().isEmpty() ||
+                textoRepetirPasswordRegistro.text.toString().isEmpty()
+    }
+    private fun checkPassword(): Boolean {
+        return textoPasswordRegistro.text.toString().equals(textoRepetirPasswordRegistro.text.toString())
     }
 }
